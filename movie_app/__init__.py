3081ccbf71f73fc157047dfaca0ce6d9d4e46ce3
Haarvish <haarvish@gmail.com>
2023-11-10 17:21:29 +0530

    render tweaking

--- movie_app/__init__.py
+++ movie_app/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_sqlalchemy  import SQLAlchemy
 from flask_migrate import Migrate
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db' 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wysxiyjypzwsha:5f1c36fc53f8a75cc88ec5974dfdc5774c85c25ea32a484a16bcefcd8db7e7e8@ec2-44-215-22-37.compute-1.amazonaws.com:5432/de3bolos0fletr'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://movie_app_9quc_user:wBqAtkECtDgcwguKSSKIVUZOJwhNJMhP@dpg-cl70smoicrhc73cvl3ng-a.oregon-postgres.render.com/movie_app_9quc'
+
+
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 from movie_app import routes
