6cf540c36f4932ab5c218b73557046394120e098
Haarvish <haarvish@gmail.com>
2023-11-10 16:28:14 +0530

    heroku tweaking

--- movie_app/__init__.py
+++ movie_app/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_sqlalchemy  import SQLAlchemy
 from flask_migrate import Migrate
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db' 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wysxiyjypzwsha:5f1c36fc53f8a75cc88ec5974dfdc5774c85c25ea32a484a16bcefcd8db7e7e8@ec2-44-215-22-37.compute-1.amazonaws.com:5432/de3bolos0fletr'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db' 
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wysxiyjypzwsha:5f1c36fc53f8a75cc88ec5974dfdc5774c85c25ea32a484a16bcefcd8db7e7e8@ec2-44-215-22-37.compute-1.amazonaws.com:5432/de3bolos0fletr'
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 from movie_app import routes
