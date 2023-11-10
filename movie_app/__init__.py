30c42bff1350eaa2956ff965b392941f00661e53
CHEVULA HAARVISH <76098768+CHEVULAHAARVISH@users.noreply.github.com>
2023-11-10 18:46:46 +0530

    Update __init__.py

--- movie_app/__init__.py
+++ movie_app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy  import SQLAlchemy
 from flask_migrate import Migrate
+import os
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 
 db = SQLAlchemy(app)
