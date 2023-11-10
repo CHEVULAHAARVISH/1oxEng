7cd147b496de8303f553dfcefaea5fe5ebaed172
Haarvish <haarvish@gmail.com>
2023-11-10 17:54:19 +0530

    tweaking for the render

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from movie_app import app,db
+from movie_app import create_app, db
+
+app = create_app()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
-
